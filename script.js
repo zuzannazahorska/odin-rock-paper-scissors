e693c3d8c54667a454913ae034640238bfdaa2b4
Zuzanna <zahorska.z96@gmail.com>
2023-09-19 14:59:09 +0200

    add simple playRound() function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const items = ["Rock", "Paper", "Scissors"];
+const items = ["rock", "paper", "scissors"];
 
 function computerChoice(arr) {
   const randomItem = Math.floor(Math.random() * arr.length);
@@ -7,5 +7,30 @@ function computerChoice(arr) {
 }
 
 const result = computerChoice(items);
-console.log(result);
 
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Tie!";
+  } else if (playerSelection === "rock" && computerSelection == "paper") {
+    return "Computer wins!";
+  } else if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You win!";
+  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+    return "Computer wins!";
+  } else if (playerSelection == "scissors" && computerSelection == "paper") {
+    return "You win!";
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "Computer wins!";
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    return "You win!";
+  } else {
+    return "Something went wrong!";
+  }
+}
+
+let playerSelection = prompt(
+  "Please enter rock, paper, or scissors"
+).toLowerCase();
+let computerSelection = computerChoice(items);
+
+console.log(playRound(playerSelection, computerSelection));
